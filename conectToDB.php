c5b58b83aedddb623f3135fcd69a2dfe39847c87
arek <bajurski@hotmail.com>
2023-01-15 13:24:13 +0100

    Going forward

--- conectToDB.php
+++ conectToDB.php
@@ -1,2 +1,14 @@
 <?php
-echo "Hello";
+try
+{
+    $pdo = new PDO('mysql:host=localhost;dbname=eur_dolar', 'root');
+    if($pdo != NULL){
+    }else {
+        echo 'PDO is empty', '<br><br>';
+    }
+}
+catch(PDOException $e)
+{
+    echo 'Połączenie nie mogło zostać utworzone: ' . $e->getMessage(), "<br>";
+}
+?>
