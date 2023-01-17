323e3e3e36a22c6216ddbf2232446cacb9a4ed91
arek <bajurski@hotmail.com>
2023-01-17 23:06:46 +0100

    One graph with two date chsers works!

--- conectToDB.php
+++ conectToDB.php
@@ -3,6 +3,7 @@ try
 {
     $pdo = new PDO('mysql:host=localhost;dbname=eur_dolar', 'root');
     if($pdo != NULL){
+        echo 'Połączenie zostało nawiązane';
     }else {
         echo 'PDO is empty', '<br><br>';
     }
--- index.php
+++ index.php
@@ -1,13 +1,46 @@
 <?php
-require_once "conectToDB.php";
-//$dataBase = new conectToDB();
+try
+{
+    $pdo = new PDO('mysql:host=localhost;dbname=eur_dolar', 'root');
+    if($pdo != NULL){
+        //echo 'Połączenie zostało nawiązane';
+    }else {
+        echo 'PDO is empty', '<br><br>';
+    }
+}
+catch(PDOException $e)
+{
+    echo 'Połączenie nie mogło zostać utworzone: ' . $e->getMessage(), "<br>";
+}
+try {
+    $sql = "SELECT * FROM eur_dolar.history";
+    $result = $pdo->query($sql);
+    if($result->rowCount()>0) {
+        while($row = $result->fetch()) {
+            $dateArray[] = $row["COL 1"];
+            $eurbuyArray[] = $row["COL 2"];
+
+            $eursellArray[] = $row["COL 3"];
+        }
+        unset($result);
+    } else {
+        echo 'No data in the database';
+    }
+} catch(PDOException $e) {
+    die("Error");
+}
+unset($pdo);
 ?>
 <!doctype html>
 <html lang="en">
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Getting Started with Chart JS with www.chartjs3.com</title>
+    <!--<style>
+       .chartBox {
+           width: 1000px;
+       }
+   </style>-->
     <style>
         * {
             margin: 0;
@@ -24,59 +57,47 @@ require_once "conectToDB.php";
             padding: 10px;
             font-size: 20px;
         }
-        .chartCard {
-            width: 100vw;
-            height: calc(100vh - 40px);
-            background: rgba(54, 162, 235, 0.2);
-            display: flex;
-            align-items: center;
-            justify-content: center;
-        }
         .chartBox {
-            width: 700px;
+            width: 900px;
             padding: 20px;
             border-radius: 20px;
             border: solid 3px rgba(54, 162, 235, 1);
             background: white;
         }
     </style>
-
 </head>
 <body>
-
-<div class="chartMenu">
-    <p>WWW.CHARTJS3.COM (Chart JS 4.1.2)</p>
-</div>
-<div class="chartCard">
-    <div class="chartBox">
-        <canvas id="myChart"></canvas>
-    </div>
+<div class = "chartBox">
+    <input type="date" onchange="startOfDay(this)" value="2020-01-01" min="2020-01-01" max="2020-11-21">
+    <input type="date" onchange="endOfDay(this)" value="2020-11-21" min="2020-01-01" max="2020-11-21">
+    <canvas id="myChart"></canvas>
 </div>
 <script type="text/javascript" src="https://cdn.jsdelivr.net/npm/chart.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/chartjs-adapter-date-fns/dist/chartjs-adapter-date-fns.bundle.min.js"></script>
+
 <script>
+    const dateArrayJS = <?php echo json_encode($dateArray)?>;
+    // console.log(dateArrayJS);
+
+    const dateChartJs = dateArrayJS.map((day,index) => {
+        let dayjs = new Date(day);
+        return dayjs.setHours(0, 0, 0, 0)
+        // return dayjs
+    });
+    console.log(dateChartJs)
     // setup
     const data = {
-        labels: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'],
+        labels: dateChartJs,
         datasets: [{
-            label: 'Weekly Sales',
-            data: [18, 12, 6, 9, 12, 3, 9],
+            label: '# EUR buy',
+            data: <?php echo json_encode($eurbuyArray);?>,
             backgroundColor: [
                 'rgba(255, 26, 104, 0.2)',
-                'rgba(54, 162, 235, 0.2)',
-                'rgba(255, 206, 86, 0.2)',
-                'rgba(75, 192, 192, 0.2)',
-                'rgba(153, 102, 255, 0.2)',
-                'rgba(255, 159, 64, 0.2)',
-                'rgba(0, 0, 0, 0.2)'
+
             ],
             borderColor: [
                 'rgba(255, 26, 104, 1)',
-                'rgba(54, 162, 235, 1)',
-                'rgba(255, 206, 86, 1)',
-                'rgba(75, 192, 192, 1)',
-                'rgba(153, 102, 255, 1)',
-                'rgba(255, 159, 64, 1)',
-                'rgba(0, 0, 0, 1)'
+
             ],
             borderWidth: 1
         }]
@@ -84,22 +105,42 @@ require_once "conectToDB.php";
 
     // config
     const config = {
-        type: 'line',
+        type: 'bar',
         data,
         options: {
             scales: {
+                x: {
+                    type: 'time',
+                    min: '2020-01-01',
+                    max: '2020-11-21',
+                    time: {
+                        unit: 'day'
+                    }
+                },
                 y: {
                     beginAtZero: true
                 }
             }
         }
     };
-
-    // render init block
     const myChart = new Chart(
         document.getElementById('myChart'),
         config
     );
+    function startOfDay(date) {
+        const startDate = new Date(date.value);
+        console.log(startDate.setHours(0, 0, 0, 0));
+        myChart.config.options.scales.x.min = startDate.setHours(0, 0, 0, 0);
+        myChart.update();
+    }
+    function endOfDay(date) {
+        const endDate = new Date(date.value);
+        console.log(endDate.setHours(0, 0, 0, 0));
+        myChart.options.scales.x.max = endDate.setHours(0, 0, 0, 0);
+        myChart.update();
+    }
+
+    // render init block
 
 </script>
 
